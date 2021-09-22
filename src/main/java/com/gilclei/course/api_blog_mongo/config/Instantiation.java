d55b117fc25b14f09b3dafdabfe357dc2b30d351
gil <gilclei@gmail.com>
2021-09-21 23:13:18 -0300

    instace database datas

---
+++ src/main/java/com/gilclei/course/api_blog_mongo/config/Instantiation.java
@@ -0,0 +1,32 @@
+package com.gilclei.course.api_blog_mongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.gilclei.course.api_blog_mongo.domain.User;
+import com.gilclei.course.api_blog_mongo.repositories.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner{
+	@Autowired
+	private UserRepository repository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		repository.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		repository.saveAll(Arrays.asList(maria,alex,bob));
+		
+		
+		
+	}
+
+}
