a1cd23379d4ff0f5d127657039ec18cfc77b89a9
gil <gilclei@gmail.com>
2021-09-21 21:27:52 -0300

    first commit

---
+++ src/main/java/com/gilclei/course/api_blog_mongo/ApiBlogMongoApplication.java
@@ -0,0 +1,13 @@
+package com.gilclei.course.api_blog_mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBlogMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiBlogMongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gilclei/course/api_blog_mongo/ApiBlogMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gilclei.course.api_blog_mongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBlogMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
